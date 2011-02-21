4625dd193ab770712a88446947321f46a126219d
Ivov Dry <ivov.dry@gmail.com>
2011-02-22 00:48:09 +0200

    Updated process creation

--- rr/rr.js
+++ rr/rr.js
@@ -3,46 +3,52 @@ var PROCESS_MAX_START = 1000;
 var PX_PER_SECOND = 20;
 var PROCESS_HEIGHT = 30;
 
-function Process(i, l, s, c) {
+function Process(i, arrival, burst, color) {
 	this.id = -1;
-	if (l < 1 || l > PROCESS_MAX_LENGTH) {
-		alert('Incorrect process lenght (1-'+ PROCESS_MAX_LENGTH +').');
+	if (arrival < 0 || arrival > PROCESS_MAX_LENGTH) {
+		alert('Incorrect process arrival time (1-'+ PROCESS_MAX_LENGTH +').');
 		return false;
 	}
-	if (s < 0 || l > PROCESS_MAX_START) {
-		alert('Incorrect process start time (0-'+ PROCESS_MAX_START +').');
+	if (burst < 1 || burst > PROCESS_MAX_START) {
+		alert('Incorrect process burst time (0-'+ PROCESS_MAX_START +').');
 		return false;
 	}
 	this.id = i;
-	this.time = l;
-	this.length = l * PX_PER_SECOND - 2;
-	this.startsAt = s;
-	this.color = c;
+	this.burst = burst;
+	this.arrival = arrival;
+	this.color = color;
 }
-Process.prototype.output = function(time_margin) {
-	return '<div id="proc_'+ this.id +'" class="pl" style="height: '+ PROCESS_HEIGHT +'px; width: '+ this.length +'px; margin-left: '+ (time_margin ) +'px; background-color: #'+ this.color +'">&nbsp;'+ this.startsAt +'</div>';
+Process.prototype.output = function() {
+	var margin = this.arrival * PX_PER_SECOND;
+	var width = this.burst * PX_PER_SECOND - 2;
+	return '<div id="proc_'+ this.id +'" class="pl" style="margin-left: '+ margin + 'px; height: '+ PROCESS_HEIGHT +'px; width: '+ width +'px; background-color: #'+ this.color +'">P'+ this.id +' ('+ this.arrival +', '+ this.burst +')</div>';
 }
 
 function ProcessList() {
 	this.processes = {};
 	this.count = 0;
 	this.colors = ['00ffff', '99ff99', 'ffcccc', 'cc99ff', 'ff66ff', 'dddddd', 'ff6666', 'ff9966'];
-	this.cur_time = 0;
+	this.max_arrival = 0;
 }
-ProcessList.prototype.addNew = function(time, startsAt) {
-	time = parseInt(time, 10);
-	startsAt = parseInt(startsAt, 10);
-	if (isNaN(time) || isNaN(startsAt)) {
+ProcessList.prototype.addNew = function(arrival, burst) {
+	arrival = parseInt(arrival, 10);
+	burst = parseInt(burst, 10);
+	if (isNaN(arrival) || isNaN(burst)) {
 		alert('Incorrect numbers.');
 		return false;
 	}
+	if (this.max_arrival > arrival) {
+		alert('Incorrect arrival time. Arrival must be >= '+ this.max_arrival);
+		return false;
+	}
 	
-	var new_id = this.count;
+	var new_id = this.count + 1;
 	var new_color = this.colors[new_id % this.colors.length];
-
-	var p = new Process(new_id, time, startsAt, new_color);
+	var p = new Process(new_id, arrival, burst, new_color);
 	if (p.id == -1) return false;
-	if (startsAt < this.cur_time) {
+	
+	this.max_arrival = arrival;
+	/*if (startsAt < this.cur_time) {
 		alert('Please add process after the last process in the list.');
 		return false;
 	}
@@ -56,9 +62,14 @@ ProcessList.prototype.addNew = function(time, startsAt) {
 		margin_time = startsAt - (prev_p.startsAt + prev_p.time);
 		margin_time = margin_time * PX_PER_SECOND;	
 	}
-	$('#ProcessList').append(p.output(margin_time));
+	*/
+	$('#ProcessList').append(p.output());
 
 	this.processes[new_id] = p;
 	this.count++;
 }
+ProcessList.prototype.startFIFS = function() {
+	
+}
+
 var pl = new ProcessList();
