8d9b98b4f6f71423798f3a668155c79f987e23a7
Nikita Cernovs <nikita.cernovs@gmail.com>
2011-03-01 22:09:09 +0200

    Code refactoring

--- first_come_first_served/first_come_first_served.js
+++ first_come_first_served/first_come_first_served.js
@@ -1,9 +1,20 @@
-ProcessList.prototype.startFIFS = function() {
-	var p;
-	for (var k in this.processes) {
-		p = this.processes[k];
-		p.work();
-	}
+$(document).ready(function() {
+  add_start_button_click_handler();
+});
+
+  
+function add_start_button_click_handler() {
+  $("#start").click(function() {
+    process_list.startFIFS();
+  });
 }
 
-// pl.startFIFS();
+
+ProcessList.prototype.startFIFS = function() {
+  var process;
+  for (var i in this.processes) {
+    process = this.processes[i];
+    process.work();
+  }
+}
+
