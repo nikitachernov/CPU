30a2493349a6e35d3cb72b59d3ff1e1f3c215a17
Nikita Cernovs <nikita.cernovs@gmail.com>
2011-03-03 15:38:14 +0200

    SJF draft

--- shortest_job_first/shortest_job_first.js
+++ shortest_job_first/shortest_job_first.js
@@ -1,39 +1,52 @@
 $(document).ready(function() {
   add_start_button_click_handler();
-  console.log("!");
 });
 
   
 function add_start_button_click_handler() {
   $("#start").click(function() {
     $("#start, #new_process").hide();
-    process_list.startSJF);
+    process_list.startSJF();
   });
 }
 
 ProcessList.prototype.startSJF = function() {
-  this.current_process = 1;
-  this.run_current_process();
-}
-
-ProcessList.prototype.run_current_process = function() {
-  var process = this.processes[this.current_process];
-  process.work()
-}
-
-ProcessList.prototype.run_next_process = function() {
-  if (this.count > this.current_process) {
-    this.current_process = this.current_process + 1;
-    this.run_current_process();
+  var process;
+  for (id in this.processes) {
+    process = this.processes[id];
+    process.terminated = 'N';
   }
+  this.run_shortest_process();
+}
+
+ProcessList.prototype.run_shortest_process = function() {
+  var shortest_process_id = 0;
+  var shortest_proces_burst = 0;
+  var process;
+  for (id in this.processes) {
+    process = this.processes[id];
+    console.log(process.terminated);
+    if (process.terminated == 'N' && (process.burst < shortest_proces_burst || shortest_proces_burst == 0)) {
+      shortest_proces_burst = process.burst;
+      shortest_process_id = process.id;
+    }
+  }
+  if (shortest_process_id > 0)
+    this.processes[shortest_process_id].work();
+}
+
+ProcessList.prototype.run_next_shortest_process = function() {
+  this.run_shortest_process();
 }
 
 Process.prototype.work = function() {
+  var process = this;
   $('#p_work_'+ this.id).animate(
     {width: '0'},
-    this.burst * 1000,
+    process.burst * 1000,
     'linear', function() {
-      process_list.run_next_process();
+      process.terminated = 'Y';
+      process_list.run_next_shortest_process();
     }
   );
 }
