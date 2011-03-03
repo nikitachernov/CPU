6f2c876ba17f2348b902481c6914ab9c2a615e7f
Nikita Cernovs <nikita.cernovs@gmail.com>
2011-03-03 14:51:50 +0200

    Created SJF structure

---
+++ shortest_job_first/shortest_job_first.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  add_start_button_click_handler();
+  console.log("!");
+});
+
+  
+function add_start_button_click_handler() {
+  $("#start").click(function() {
+    $("#start, #new_process").hide();
+    process_list.startFIFS();
+  });
+}
+
+ProcessList.prototype.startFIFS = function() {
+  this.current_process = 1;
+  this.run_current_process();
+}
+
+ProcessList.prototype.run_current_process = function() {
+  var process = this.processes[this.current_process];
+  process.work()
+}
+
+ProcessList.prototype.run_next_process = function() {
+  if (this.count > this.current_process) {
+    this.current_process = this.current_process + 1;
+    this.run_current_process();
+  }
+}
+
+Process.prototype.work = function() {
+  $('#p_work_'+ this.id).animate(
+    {width: '0'},
+    this.burst * 1000,
+    'linear', function() {
+      process_list.run_next_process();
+    }
+  );
+}
