99031fc680f768226f64e9409adb125cce0e6fbd
Nikita Chernov <nikita.cernovs@gmail.com>
2011-03-05 15:32:25 +0200

    Code tidyup

--- first_come_first_served/first_come_first_served.js
+++ first_come_first_served/first_come_first_served.js
@@ -1,35 +1,42 @@
-
 $(document).ready(function() {
-  add_start_button_click_handler();
+    add_start_button_click_handler();
 });
 
 function add_start_button_click_handler() {
-  $("#start").click(function() {
-    $("#start, #new_process").hide();
-    process_list.startFCFS();
-  });
+    $("#start").click(function() {
+        $("#start, #new_process").hide();
+        process_list.startFCFS();
+    });
 }
 
 
 ProcessList.prototype.startFCFS = function() {
-  var result = {};
-  var timer = 0;
-  var last_time = 0;
-/*
-[
-0: {'type': 'process', 'id' : 5, 	start: 0, end: 5,  color: },
-5: {'type': 'empty', 				start: 5, end: 15, color: },
-]
-*/
-  for (var k in this.processes) {
-	var proc = this.processes[k];
-	if (timer < proc.arrival) {
-		result[timer] = {'type': 'empty', 'start': timer, 'end': proc.arrival, 'color': 'gray'};
-		timer = proc.arrival;
-	}
-	result[timer] = {'type': 'proc', 'id' : proc.id, 'start': timer, 'end': (timer + proc.burst), 'color': proc.color};
-	timer += proc.burst;
-	last_time = timer;
-  }
-  this.animate(result, last_time);
+    var result = {};
+    var timer = 0;
+    var last_time = 0;
+
+    /*[0: {'type': 'process', 'id' : 5, start: 0, end: 5,  color: },
+       5: {'type': 'empty',             start: 5, end: 15, color: }]*/
+    for (var k in this.processes) {
+        var proc = this.processes[k];
+        if (timer < proc.arrival) {
+            result[timer] = {
+                'type': 'empty',
+                'start': timer,
+                'end': proc.arrival,
+                'color': 'gray'
+            };
+            timer = proc.arrival;
+        }
+        result[timer] = {
+            'type': 'proc',
+            'id': proc.id,
+            'start': timer,
+            'end': (timer + proc.burst),
+            'color': proc.color
+        };
+        timer += proc.burst;
+        last_time = timer;
+    }
+    this.animate(result, last_time);
 }
